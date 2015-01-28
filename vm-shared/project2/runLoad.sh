0c992723eb89eb50d4553164b6443e76f21594e6
Lowell Bander <lowellbander@gmail.com>
2015-01-28 15:46:03 -0800

    removes duplicates

--- vm-shared/project2/runLoad.sh
+++ vm-shared/project2/runLoad.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # Run the drop.sql batch file to drop existing tables
-# Inside the drop.sql, you sould check whether the table exists. Drop them ONLY
-# if they exists.
 mysql CS144 < drop.sql
 
 # Run the create.sql batch file to create the database and tables
@@ -11,15 +9,25 @@ mysql CS144 < create.sql
 # Compile and run the parser to generate the appropriate load files
 ant
 ant run-all
-...
+#...
 
 # If the Java code does not handle duplicate removal, do this now
-sort ...
-...
+user="user.del"
+location="location.del"
+item="item.del"
+bid="bid.del"
+category="category.del"
+
+(sort $user | uniq -u) > $user
+(sort $location | uniq -u) > $location
+(sort $item | uniq -u) > $item
+(sort $bid | uniq -u) > $bid
+(sort $category | uniq -u) > $category
 
 # Run the load.sql batch file to load the data
 mysql CS144 < load.sql
 
 # Remove all temporary files
-rm ...
-...
+# rm ...
+# ...
+
