6435f207e15b7df334be91bdb89e02c055225d04
Lowell Bander <lowellbander@gmail.com>
2015-03-02 15:07:51 -0800

    moved things and updates master build script

--- vm-shared/run.sh
+++ vm-shared/run.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-cd project2 &&
+cd project2/ &&
 bash runLoad.sh &&
 cd .. &&
 
 ant -f project3-indexer/build.xml run &&
 ant -f project3-searcher/build.xml run &&
 
-cd project3-searcher &&
+cd project3-searcher/ &&
 bash deploy.sh &&
+cd .. &&
+
+cd project-4/ &&
+bash run.sh &&
 cd .. &&
 
 echo "Done."
