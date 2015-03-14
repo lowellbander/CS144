0a1076944301897cb7e6eb9d2a4aa5cd8b033ada
Akshay Bakshi <bakshiakshay94@gmail.com>
2015-03-14 15:06:08 -0700

    Confirmation works

--- vm-shared/project5/WebContents/confirmation.jsp
+++ vm-shared/project5/WebContents/confirmation.jsp
@@ -1,5 +1,13 @@
 <html>
-	<body>
-		Hello, world. This is the confirmation page.
-	</body>
+	<title>Confirmation</title>
+    <body>
+	    <h2>Confirmation</h2>
+        	<ul>
+        		<li>Item ID: <%= request.getAttribute("itemID") %></li>
+             	<li>Name: <%= request.getAttribute("itemName") %></li>
+                <li>Buy Price: <%= request.getAttribute("buyPrice") %></li> 
+                <li>Credit Card No.: <%= request.getAttribute("creditCardNum") %></li>
+                <li>Date &amp; Time: <%= request.getAttribute("dateTime") %></li>
+            </ul>
+    </body>
 </html>
--- vm-shared/project5/src/edu/ucla/cs/cs144/ConfirmationServlet.java
+++ vm-shared/project5/src/edu/ucla/cs/cs144/ConfirmationServlet.java
@@ -5,14 +5,29 @@ import javax.servlet.ServletException;
 
 import javax.servlet.Servlet;
 import javax.servlet.http.*;
+import java.text.DateFormat;
+import java.util.Date;
 
 public class ConfirmationServlet extends HttpServlet implements Servlet {
   public ConfirmationServlet() {}
-  protected void doGet (HttpServletRequest request,
+  protected void doPost (HttpServletRequest request,
                         HttpServletResponse response) throws ServletException,
                                                              IOException {
 
     // DO STUFF
+    HttpSession session = request.getSession(true);
+    String itemID = (String)session.getAttribute("itemID");
+    String name = (String)session.getAttribute("itemName");
+    String buyPrice = (String)session.getAttribute("buyPrice");
+    String creditCardNum = (String)request.getParameter("creditCardNum");
+    Date now = new Date();
+
+    request.setAttribute("itemID", itemID);
+    request.setAttribute("itemName", name);
+    request.setAttribute("buyPrice", buyPrice);
+    request.setAttribute("creditCardNum", creditCardNum);
+    request.setAttribute("dateTime", DateFormat.getDateTimeInstance(
+            DateFormat.LONG, DateFormat.LONG).format(now));
 
     request.getRequestDispatcher("/confirmation.jsp")
            .forward(request, response);
